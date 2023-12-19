f6e1808de4997279cce7981831c3a73879ef8a1e
98noble <98noble@naver.com>
2023-12-19 20:24:18 +0900

    add: controller

---
+++ src/main/java/com/portfolio/project/controller/BannerController.java
@@ -0,0 +1,24 @@
+package com.portfolio.project.controller;
+
+import com.portfolio.project.service.BannerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BannerController {
+
+    @Autowired
+    private BannerService bannerService;
+
+    @GetMapping ("/banners")
+    public List<String> listBanner() throws Exception {
+
+        return bannerService.listBanner();
+    }
+}
